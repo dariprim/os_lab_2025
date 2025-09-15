44157fc33b532f094a872f4dd8a62b41c8d0e653
dariprim <dasha.primak.2014@mail.ru>
2025-09-15 16:44:59 +0000

    Добавлены статическая и динамическая библиотеки для revert, залинкованы в main.c, также изменен метод в swap.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,23 @@
 
 void RevertString(char *str)
 {
-	// your code here
+    // два указателя для начала и конца, будем двигать их к друг другу
+    char *start = str;
+    char *end = str;
+    
+    // Находим конец строки
+    while (*end != '\0') {
+        end++;
+    }
+    end--; // Возвращаемся к последнему символу перед '\0'
+    
+    // Меняем символы местами
+    while (start < end) {
+        char temp = *start;
+        *start = *end;
+        *end = temp;
+        
+        start++;
+        end--;
+    }
 }
-
